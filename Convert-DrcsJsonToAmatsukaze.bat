f01f56a418a557565a2c768ccf502ed454fb2df1
seijit <seijit@users.noreply.github.com>
2025-12-09 21:07:08 +0900

    Update execution policy in batch script

--- Convert-DrcsJsonToAmatsukaze.bat
+++ Convert-DrcsJsonToAmatsukaze.bat
@@ -2,8 +2,8 @@
 chcp 65001 > nul
 
 :: 自身と同名のps1スクリプトに、ドラッグされた引数を渡して実行 
-set "SELF_FILE_NAME=%~n0"
 title %~nx0
-pwsh -NoProfile -ExecutionPolicy RemoteSigned -File "%~dp0%SELF_FILE_NAME%.ps1" %*
+set "SELF_FILE_NAME=%~n0"
+pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0%SELF_FILE_NAME%.ps1" %*
 
 pause
