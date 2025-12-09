b0411dca7c6e62e2533c705748a7173851087d49
seijit <seijit@users.noreply.github.com>
2025-12-09 23:53:41 +0900

    Update comment statement

--- Convert-DrcsJsonToAmatsukaze.bat
+++ Convert-DrcsJsonToAmatsukaze.bat
@@ -16,21 +16,22 @@ REM
 REM  2. コマンドプロンプトから実行 (オプション指定) 
 REM     このバッチファイルの後ろにパラメータを付けることで、設定を変更できます。 
 REM
-REM     [例A] SD放送やワンセグ(低画質)のデータも含める場合: 
-REM       Convert-DrcsJsonToAmatsukaze.bat -IncludeNonHD
+REM     [例A] 既存のマップファイルを引き継いで差分更新する場合: 
+REM       Convert-DrcsJsonToAmatsukaze.bat -ExistingMapPath "C:\amatsukaze\drcs\drcs_map.txt"
+REM       Convert-DrcsJsonToAmatsukaze.bat "C:\amatsukaze\drcs\drcs_map.txt" -OutputDir
 REM
 REM     [例B] 出力先フォルダ名を変更する場合 (※フォルダは存在しないこと): 
 REM       Convert-DrcsJsonToAmatsukaze.bat -OutputDir "drcs_v2"
 REM
-REM     [例C] 既存のマップファイルを引き継いで差分更新する場合: 
-REM       Convert-DrcsJsonToAmatsukaze.bat -ExistingMapPath "C:\amatsukaze\drcs\drcs_map.txt" -OutputDir "drcs_v3"
+REM     [例C] SD放送やワンセグ(低画質)のデータも含める場合: 
+REM       Convert-DrcsJsonToAmatsukaze.bat -IncludeNonHD
 REM
 REM -----------------------------------------------------------------------------
 
 :: 文字コード設定 
 chcp 65001 > nul
 
-:: 自身と同名のps1スクリプトに、ドラッグされた引数を渡して実行 
+:: 自身と同名のps1スクリプトに、自身に渡された引数をすべて渡して実行 
 title %~nx0
 set "SELF_FILE_NAME=%~n0"
 powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0%SELF_FILE_NAME%.ps1" %*
